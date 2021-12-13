ab15685e7af219bdbd608b4c68173f849eef3e57
Muhammad Irtiza <m7.irtiza@gmail.com>
2021-12-13 09:26:19 +0500

    Book Keeping Array functionality and refactored code.

--- Assignment 5/k_mean_functions.py
+++ Assignment 5/k_mean_functions.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import cv2
 from random import randint
 
-def distance(point_1, point_2):
-    x1, y1, z1 = point_1[0], point_1[1], point_1[2] 
-    x2, y2, z2 = point_2[0], point_2[1], point_2[2]
-    dist = ((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2)
+def euclidean_distance(point_1, point_2):
+    dist = 0
+    for (coordinate_1, coordinate_2) in zip(point_1, point_2):
+        dist += (int(coordinate_1) - int(coordinate_2)) ** 2
+
     return np.sqrt(dist)
 
-def random_means(image, k, image_height, image_width):
+def initialize_random_means(image, k, image_height, image_width):
     means = []
     for _ in range(k):
         rand_1 = randint(0, image_height - 1)
@@ -20,11 +21,11 @@ def random_means(image, k, image_height, image_width):
         means.append(pixel)
 
     return means
-        
-def select_minimum_distance_mean(distances):
+
+def find_nearest_mean(distances):
     minimum = min(distances)
     minimum_distance_mean = distances.index(minimum)
     return minimum_distance_mean
 
-def update_book_keeping_array():
+def update_means(book_keeping_array, ):
     pass
--- Assignment 5/main.py
+++ Assignment 5/main.py
@@ -2,7 +2,7 @@ from PIL.Image import MAX_IMAGE_PIXELS
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-import helping_functions as hf
+import k_mean_functions as KM
 
 IMAGE_PATH = r'./face.jpg'
 MAX_ITERATIONS = 10
@@ -14,22 +14,16 @@ image_height, image_width, _ = image_RGB.shape
 # plt.show() 
 
 k = int(input("Number of Colors> "))
-k_means_list = hf.random_means(image_RGB, k, image_height, image_width)
-
+k_means_list = KM.initialize_random_means(image_RGB, k, image_height, image_width)
 book_keeping_array = np.zeros((image_height, image_width))
 
 for _ in range(MAX_ITERATIONS):
-    
-    for image_row in image_RGB:
-        
-        for image_pixel in image_row:
+    for row_index, image_row in enumerate(image_RGB):
+        for pixel_index, pixel in enumerate(image_row):
             
             distances_from_means = []
             for mean in k_means_list:
-                print(mean)
-                distances_from_means.append(hf.distance(image_pixel, mean))
-
-            print("\n",distances_from_means)
-            break
-        break
-    break
+                distances_from_means.append(KM.euclidean_distance(pixel, mean))
+            
+            nearest_mean = KM.find_nearest_mean(distances_from_means)
+            book_keeping_array[row_index][pixel_index] = nearest_mean
